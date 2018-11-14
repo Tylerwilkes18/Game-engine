6b4b0e773b2d6eb0f29a1c782cb5f6c413076ce0
MMB938 <MMB938@ME-101-8E76D789.isdads.salford.ac.uk>
2018-11-14 12:55:10 +0000

    Initial Main

---
+++ GameEngineWorkspace/r/src/Main.java
@@ -0,0 +1,36 @@
+import processing.core.PApplet;
+import simple_platformer.Launcher;
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("Main");
+	}
+	public Launcher launcher;
+	public void setup() {
+		background(0);
+		launcher = new Launcher(this);
+		
+	}
+	public void draw() {
+		launcher.UpdateAll();
+	}
+	public void settings() {
+		size(640,480);
+	}
+	// event handling
+	public void keyPressed()    { 
+		launcher.keyPressed(key, keyCode);
+		}
+	public void keyReleased()   { 
+		launcher.keyReleased(key, keyCode); 
+		}
+	public void mousePressed()   { 
+		launcher.mousePressed(); 
+		}
+//	void mouseMoved()    { launcher.mouseMoved(mouseX, mouseY); }
+//	void mousePressed()  { SoundManager.clicked(mouseX,mouseY); activeScreen.mousePressed(mouseX, mouseY, mouseButton); }
+//	void mouseDragged()  { launcher.mouseDragged(mouseX, mouseY, mouseButton); }
+//	void mouseReleased() { launcher.mouseReleased(mouseX, mouseY, mouseButton); }
+//	void mouseClicked()  { launcher.mouseClicked(mouseX, mouseY, mouseButton); }
+
+}
